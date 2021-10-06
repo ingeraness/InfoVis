ab6c2681258f516ce37516cd44b2b804da72bf39
Inger-Ane Schefte <inger-aness@hotmail.com>
2021-10-06 14:03:34 +0100

    remove items with missing values

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-hfi_cc_df = pd.read_csv("hfi_cc_2020.csv", delimiter=',', encoding='latin1')
+hfi_cc_df = pd.read_csv("src/hfi_cc_2020.csv", delimiter=',', encoding='latin1')
 
 data = hfi_cc_df[['ISO_code', 'year', 'countries', 'region', 'hf_score', 'hf_rank', 'pf_ss_disappearances_violent', 'pf_ss', 'pf_ss_women', 'ef_legal_police']]
 #data[data['region']=="Europe"]
@@ -10,6 +10,7 @@ data = hfi_cc_df[['ISO_code', 'year', 'countries', 'region', 'hf_score', 'hf_ran
 europe_df = data.loc[data['region'].isin(['Eastern Europe', 'Western Europe'])]
 
 europe_df.dropna(subset=['hf_score'], inplace=True)
+europe_df.dropna(subset=['ef_legal_police'], inplace=True)
 
 print(europe_df)
 
