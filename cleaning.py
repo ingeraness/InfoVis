188a30a48ab8cac3b4e14a4df5879ccbd97c8899
ingeraness <56219236+ingeraness@users.noreply.github.com>
2021-10-06 13:08:45 +0100

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+hfi_cc_df = pd.read_csv("hfi_cc_2020.csv", delimiter=',', encoding='latin1')
+
+data = hfi_cc_df[['ISO_code', 'year', 'countries', 'region', 'hf_score', 'hf_rank', 'pf_ss_disappearances_violent', 'pf_ss', 'pf_ss_women', 'ef_legal_police']]
+#data[data['region']=="Europe"]
+
+#data.loc[data['region'] == 'Europe']
+
+europe_df = data.loc[data['region'].isin(['Eastern Europe', 'Western Europe'])]
+
+europe_df.dropna(subset=['hf_score'], inplace=True)
+
+print(europe_df)
+
+europe_df.to_csv('data.csv', index=False, header=True)
