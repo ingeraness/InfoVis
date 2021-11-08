363410b143b8b44628725810b8c416b3a87bf928
Solveig Grimstad <solveig.b.grimstad@gmail.com>
2021-11-08 14:04:25 +0000

    years

--- script/events.js
+++ script/events.js
@@ -43,19 +43,19 @@ function handleMouseOver(event, d) {
     .style("fill", "red");
 
   scatterPlot
-    .selectAll("circle")
-    //.selectAll("dataScatter")
+    .selectAll("circle#dataScatter")
+    // .selectAll("#dataScatter")
     .filter(function (b) {
       if (d.country == b.country) {
-        console.log("Info om dette landet: " + b.country);
-        console.log("Year: " + b.year);
-        console.log("Freedom index: " + b.hf_score);
-        console.log("Freedom rank: " + b.hf_rank);
-        console.log("Women’s Freedom: " + b.pf_ss_women);
-        console.log("Security and Safety: " + b.pf_ss);
-        console.log("Police Reliability: " + b.ef_legal_police);
-        console.log("Criminal trends: " + b.pf_ss_disappearances_violent);
-        console.log("Religious Freedom: " + b.pf_religion_freedom);
+        // console.log("Info om dette landet: " + b.country);
+        // console.log("Year: " + b.year);
+        // console.log("Freedom index: " + b.hf_score);
+        // console.log("Freedom rank: " + b.hf_rank);
+        // console.log("Women’s Freedom: " + b.pf_ss_women);
+        // console.log("Security and Safety: " + b.pf_ss);
+        // console.log("Police Reliability: " + b.ef_legal_police);
+        // console.log("Criminal trends: " + b.pf_ss_disappearances_violent);
+        // console.log("Religious Freedom: " + b.pf_religion_freedom);
         return b;
       }
     })
@@ -81,10 +81,10 @@ function handleMouseOver(event, d) {
   
     );
    
-    console.log(d.chosenAttributeX);
-    console.log(chosenAttributeX);
-    console.log(d.pf_religion_freedom);
-    console.log(d);
+    // console.log(d.chosenAttributeX);
+    // console.log(chosenAttributeX);
+    // console.log(d.pf_religion_freedom);
+    // console.log(d);
 
 }
 
@@ -131,7 +131,7 @@ function handleMouseLeave(event, d) {
         return b;
       }
     })
-    .style("fill", "purple");
+    .style("fill", (d) => (d.country == chosenCountry1 ? "purple" : "green"));
 
   d3.select("div#barChart")
     .select("svg")
@@ -142,14 +142,14 @@ function handleMouseLeave(event, d) {
 // Change selected country when bar is clicked on in bar chart
 function handleClickBarChart(event, d) {
   chosenCountry1 = d.country;
-  document.getElementById("optionC1").innerHTML = chosenCountry1;
+  document.getElementById("dropdown_country1").value = chosenCountry1;
   saveDropdownCountry(3);
 }
 
 // Change selected country when bar is clicked on in scatterplot
 function handleClickScatterplot(event, d) {
   chosenCountry1 = d.country;
-  document.getElementById("optionC1").innerHTML = chosenCountry1;
+  document.getElementById("dropdown_country1").value = chosenCountry1;
   saveDropdownYear();
 }
 
@@ -163,9 +163,9 @@ function markSelectedCountries() {
     .selectAll("circle#dataScatter")
     .style("fill", "steelblue")
     .filter(function (b) {
-      console.log("COUNTRY: " + b.country);
-      console.log("COUNTRY1: " + chosenCountry1);
-      console.log("COUNTRY2: " + chosenCountry2);
+      // console.log("COUNTRY: " + b.country);
+      // console.log("COUNTRY1: " + chosenCountry1);
+      // console.log("COUNTRY2: " + chosenCountry2);
       if (b.country == chosenCountry1 || b.country == chosenCountry2) {
         return b;
       }
--- script/lineChart.js
+++ script/lineChart.js
@@ -1,6 +1,7 @@
 var country;
 var dataSet;
 
+
 function createLineChart(data, update, attribute, div) {
   margin = { top: 20, right: 20, bottom: 20, left: 40 };
   width = 500;
@@ -27,14 +28,21 @@ function createLineChart(data, update, attribute, div) {
   document.getElementById(headerString).innerHTML = labelsDict[attribute];
 
   var dataC1 = data.filter(function (d) {
-    if (d.country == chosenCountry1) {
+    if ((d.country == chosenCountry1) && (d.year>= chosenYear && d.year<=chosenYear2)){
       return d;
     }
   });
 
   var dataC2 = data.filter(function (d) {
-    if (d.country == chosenCountry2) {
+    if ((d.country == chosenCountry2) && (d.year>= chosenYear && d.year<=chosenYear2)) {
       return d;
+    }
+  });
+
+  var yearsChosen = data.filter(function(d){
+    if(d.year>= chosenYear && d.year<=chosenYear2 && d.country=="Portugal"){
+      return d;
+
     }
   });
 
@@ -46,7 +54,7 @@ function createLineChart(data, update, attribute, div) {
 
   x = d3
     .scaleLinear()
-    .domain(d3.extent(data, (d) => d.year))
+    .domain(d3.extent(yearsChosen, (d) => d.year))
     .range([margin.left, width - margin.right]);
 
   y = d3
@@ -59,7 +67,8 @@ function createLineChart(data, update, attribute, div) {
       d3
         .axisBottom(x)
         .tickFormat((x) => x)
-        .tickSizeOuter(0)
+        .ticks(yearsChosen.length - 1)
+        //.tickSizeOuter(0)
     );
 
   yAxis = (g) =>
@@ -128,6 +137,7 @@ function createLineChart(data, update, attribute, div) {
     .attr("cy", (d) => y(d[attributesDict[attribute]]))
     .attr("r", 3)
     .attr("id", "one")
+    .on("mousemove", handleMouseMove)
     .on("mouseover", handleMouseOver)
     .on("mouseleave", handleMouseLeave);
 
@@ -154,6 +164,7 @@ function createLineChart(data, update, attribute, div) {
     .attr("cy", (d) => y(d[attributesDict[attribute]]))
     .attr("r", 3)
     .attr("id", "two")
+    .on("mousemove", handleMouseMove)
     .on("mouseover", handleMouseOver)
     .on("mouseleave", handleMouseLeave);
 
